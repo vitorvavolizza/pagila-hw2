7aaa9e27ac62da0c25ffefb18896fa2d741bbfeb
vitor vavolizza <vitorvavolizza@gmail.com>
2024-02-27 22:24:16 -0800

    added 18

--- sql/18.sql
+++ sql/18.sql
@@ -7,23 +7,39 @@
  * The `to_char` function can be used to achieve the correct formatting of your percentage.
  * See: <https://www.postgresql.org/docs/current/functions-formatting.html#FUNCTIONS-FORMATTING-EXAMPLES-TABLE>
  */
-
-select rank, title, revenue, "total revenue",
-to_char(100 * "total revenue" / (
+select
+  rank,
+  title,
+  revenue,
+  "total revenue",
+  to_char(100 * "total revenue" / (
     select sum(amount)
     from film
-    left join inventory using (film_id)
-    left join rental using (inventory_id)
-    left join payment using (rental_id)
-), 'FM999.99') as "percent revenue"
-from ( select rank() over ( order by sum(case when amount is null then 0.00 else amount end) desc ) as rank,
-    film.title as title,
-    sum(case when amount is null then 0.00 else amount end) as revenue,
-    sum(sum(case when amount is null then 0.00 else amount end)) over (order by sum(case when amount is null then 0.00 else amount end) desc) as "total revenue"
+    left join inventory on inventory.film_id = film.film_id
+    left join rental on rental.inventory_id = inventory.inventory_id
+    left join payment on payment.rental_id = rental.rental_id
+  ), 'FM900.00') as "percent revenue"
+from (
+  select
+    rank,
+    title,
+    revenue,
+    sum(revenue) over (order by revenue desc) as "total revenue"
+  from (
+    select
+      rank() over (
+        order by sum(coalesce(amount, 0.00)) desc
+      ) as rank,
+      film.title as title,
+      sum(coalesce(amount, 0.00)) as revenue
     from film
-    left join inventory using (film_id)
-    left join rental using (inventory_id)
-    left join payment using (rental_id)
+    left join inventory on inventory.film_id = film.film_id
+    left join rental on rental.inventory_id = inventory.inventory_id
+    left join payment on payment.rental_id = rental.rental_id
     group by film.title
-) as seventeen
-order by rank;
+    order by revenue desc
+  ) sixteen
+) seventeen
+group by 1, 2, 3, 4
+order by rank, title;
+
